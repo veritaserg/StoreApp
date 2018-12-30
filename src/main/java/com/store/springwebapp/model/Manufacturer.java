064e0fbfac38ba5064d53cf2124faac4c17afbe9
veritaserg <infophotomasterscomua@gmail.com>
2018-12-30 23:30:57 +0200

    update

--- src/main/java/com/store/springwebapp/model/Manufacturer.java
+++ src/main/java/com/store/springwebapp/model/Manufacturer.java
@@ -1,4 +1,28 @@
 package com.store.springwebapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "manufacturer")
 public class Manufacturer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy="manufacturer", cascade = CascadeType.ALL)
+    private Set<Product> products;
+
+
 }
--- src/main/java/com/store/springwebapp/model/Product.java
+++ src/main/java/com/store/springwebapp/model/Product.java
@@ -1,14 +1,41 @@
 package com.store.springwebapp.model;
 
-import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "product")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 private Long id;
+
+    @Column(name = "name")
 private String name;
+
+    @Column(name = "cost")
     private BigDecimal cost;
+
+    @ManyToOne
+    @JoinColumn(name="manufacturer_id", nullable=false)
     private Manufacturer manufacturer;
 
+    @Column(name = "dateTime")
+private LocalDateTime dateTime;
+
+    @Column(name = "term")
+private int term;
 
 
 }
