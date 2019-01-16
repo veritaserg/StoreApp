9eb4847220c3496c7c8703adb46e52c5e941b5ff
veritaserg <infophotomasterscomua@gmail.com>
2019-01-16 14:12:54 +0200

    update

--- src/main/java/com/store/springwebapp/controller/UserController.java
+++ src/main/java/com/store/springwebapp/controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController {
 
 
 
-    @RequestMapping(value = {"/login","/"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
     public String login(Model model, String error, String logout) {
 
         if (error != null) {
