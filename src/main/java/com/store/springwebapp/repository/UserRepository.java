582a1cb2167c2ad48a8feb04c516a98c0deb617c
veritaserg <infophotomasterscomua@gmail.com>
2020-04-02 15:13:47 +0300

    update

--- src/main/java/com/store/springwebapp/repository/UserRepository.java
+++ src/main/java/com/store/springwebapp/repository/UserRepository.java
@@ -2,14 +2,16 @@ package com.store.springwebapp.repository;
 
 import com.store.springwebapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     @SuppressWarnings("unchecked")
     User save(User user);
 
+    @Query("FROM User WHERE username = :username")
     Optional<User> findByUsername(String username);
 }
